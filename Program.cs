7f5f098908060baa4d18316427dd07656d504bf5
Dai Nguyen Huy <dainh@smartosc.com>
2023-06-29 10:54:47 +0700

    Add swagger ui

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-﻿using WebApi.Authorization;
+﻿using Microsoft.OpenApi.Models;
+using WebApi.Authorization;
 using WebApi.Helpers;
 using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // add services to DI container
 {
@@ -16,9 +21,47 @@ var builder = WebApplication.CreateBuilder(args);
     // configure DI for application services
     services.AddScoped<IJwtUtils, JwtUtils>();
     services.AddScoped<IUserService, UserService>();
+
+
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestWebApi", Version = "v1" });
+
+        c.AddSecurityDefinition(JwtAuthenticationDefaults.AuthenticationScheme,
+        new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme.",
+            Name = JwtAuthenticationDefaults.HeaderName, // Authorization
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = JwtAuthenticationDefaults.AuthenticationScheme
+                    }
+                },
+                new List<string>()
+            }
+        });
+    });
 }
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // configure HTTP request pipeline
 {
@@ -34,4 +77,6 @@ var app = builder.Build();
     app.MapControllers();
 }
 
-app.Run("http://localhost:4000");
+
+//app.Run("http://localhost:4000");
+app.Run();
